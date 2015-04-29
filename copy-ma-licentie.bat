a3b0e05f29ed020d63d2266c04c8bb31188a3e05
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2015-04-29 15:55:01 +0200

    Start of L4 branch

---
+++ copy-ma-licentie.bat
@@ -0,0 +1,4 @@
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel\Acumulus\changelog-4.x.txt D:\Projecten\Acumulus\Webkoppelingen\changelog-4.x.txt
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel\Acumulus\license.txt       D:\Projecten\Acumulus\Webkoppelingen\license.txt
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel\Acumulus\licentie-nl.pdf   D:\Projecten\Acumulus\Webkoppelingen\licentie-nl.pdf
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel\Acumulus\readme-nl.txt     D:\Projecten\Acumulus\Webkoppelingen\readme-nl.txt
---
+++ copy-ma.bat
@@ -0,0 +1,4 @@
+@echo off
+mklink /J D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel\Acumulus\libraries\Siel D:\Projecten\Acumulus\Webkoppelingen\Library\Siel
+mklink /J D:\Projecten\Acumulus\Magento\www\app\code\community\Siel D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\code\community\Siel
+mklink /H D:\Projecten\Acumulus\Magento\www\app\etc\modules\Siel_Acumulus.xml D:\Projecten\Acumulus\Webkoppelingen\Magento\acumulus\app\etc\modules\Siel_Acumulus.xml
