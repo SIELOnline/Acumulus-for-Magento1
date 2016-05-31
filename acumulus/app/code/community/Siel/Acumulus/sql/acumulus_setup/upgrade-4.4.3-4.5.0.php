bee16e2ec421dcd318582b6487773e1dce93a67a
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-05-31 12:04:54 +0200

    4.5.0 upgrade

---
+++ acumulus/app/code/community/Siel/Acumulus/sql/acumulus_setup/upgrade-4.4.3-4.5.0.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @var Siel_Acumulus_Model_Resource_Setup $this */
+$installer = $this;
+$installer->startSetup();
+
+/** @var Siel_Acumulus_Helper_Data $helper */
+$helper = Mage::helper('acumulus');
+$helper->getAcumulusConfig()->upgrade('4.5.0');
+
+$installer->endSetup();
