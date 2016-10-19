e652bc152bf92f7c1aed300969eed6c79c9bae1c
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-10-19 18:21:27 +0200

    Reformatting

--- acumulus/app/code/community/Siel/Acumulus/Model/Entry.php
+++ acumulus/app/code/community/Siel/Acumulus/Model/Entry.php
@@ -1,4 +1,4 @@
-<?php /** @noinspection PhpLanguageLevelInspection */
+<?php
 
 /**
  * Class Siel_Acumulus_Model_Entry Acumulus is used by
@@ -20,27 +20,38 @@
  */
 class Siel_Acumulus_Model_Entry extends Mage_Core_Model_Abstract {
 
-  /**
-   * Magento "internal constructor" not receiving any parameters.
-   */
-  protected function _construct() {
-    $this->_init('acumulus/entry');
-  }
+    /**
+     * Magento "internal constructor" not receiving any parameters.
+     */
+    protected function _construct() {
+        $this->_init('acumulus/entry');
+    }
 
-  /**
-   * Overrides the save() method to clear the created column and set the updated
-   * column before being written to the database. The created timestamp is set
-   * by the database and should not be set by the application. As MySQl < 5.6.5
-   * only allows one timestamp with a default value, we do set the updated
-   * timestamp in code (http://stackoverflow.com/a/17498167/1475662).
-   *
-   * @return $this
-   */
-  public function save() {
-    $this
-      ->setUpdated(Mage::app()->getLocale()->storeTimeStamp())
-      ->unsetData('created');
-    return parent::save();
-  }
+    /**
+     * Overrides the save() method to clear the created column and set the
+     * updated column before being written to the database. The created
+     * timestamp is set by the database and should not be set by the
+     * application. As MySQl < 5.6.5 only allows one timestamp with a default
+     * value, we do set the updated timestamp in code
+     * (http://stackoverflow.com/a/17498167/1475662).
+     *
+     * @return $this
+     */
+    public function save() {
+        $this
+            ->setUpdated(Mage::app()->getLocale()->storeTimeStamp())
+            ->unsetData('created');
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return parent::save();
+    }
 
+    /**
+     * Get collection instance
+     *
+     * @return Siel_Acumulus_Model_Resource_Entry_Collection
+     */
+    public function getResourceCollection() {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return parent::getResourceCollection();
+    }
 }
--- acumulus/app/code/community/Siel/Acumulus/Model/Resource/Entry.php
+++ acumulus/app/code/community/Siel/Acumulus/Model/Resource/Entry.php
@@ -5,11 +5,10 @@
  */
 class Siel_Acumulus_Model_Resource_Entry extends Mage_Core_Model_Resource_Db_Abstract {
 
-  /**
-   * Magento "internal constructor" not receiving any parameters.
-   */
-  protected function _construct() {
-    $this->_init('acumulus/entry', 'entity_id');
-  }
-
+    /**
+     * Magento "internal constructor" not receiving any parameters.
+     */
+    protected function _construct() {
+        $this->_init('acumulus/entry', 'entity_id');
+    }
 }
