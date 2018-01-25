a57cd8efe3a919e25126fc6236317efa00bf69e0
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-01-25 21:55:07 +0100

    5.0.5: Restructure to (eventually) publish on Magento Marketplace; only module itself remains in this repository.

--- copy-ma1-example-to-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir D:\Projecten\Acumulus\Magento\www\app\code\community\Siel 2> nul
-rmdir /s /q D:\Projecten\Acumulus\Magento\www\app\code\community\Siel\AcumulusCustomiseInvoice 2> nul
-mklink /J D:\Projecten\Acumulus\Magento\www\app\code\community\Siel\AcumulusCustomiseInvoice D:\Projecten\Acumulus\Webkoppelingen\Magento1\acumulus-customise-invoice\app\code\community\Siel\AcumulusCustomiseInvoice
-del D:\Projecten\Acumulus\Magento\www\app\etc\modules\Siel_AcumulusCustomiseInvoice.xml 2> nul
-mklink /H D:\Projecten\Acumulus\Magento\www\app\etc\modules\Siel_AcumulusCustomiseInvoice.xml D:\Projecten\Acumulus\Webkoppelingen\Magento1\acumulus-customise-invoice\app\etc\modules\Siel_AcumulusCustomiseInvoice.xml
